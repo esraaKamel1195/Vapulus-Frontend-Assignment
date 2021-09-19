33734c9b82d5c16f0b8ee75f528df099ae822b00
Esraa Kamel <esraa.kamel1811@gmail.com>
2021-09-20 01:29:22 +0200

    Enhancements

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener  } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { WookieService } from 'src/app/services/movies.service';
 import { WookieMovie } from 'src/app/models/wookie-movie.model';
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private route: Router,
-    private wookieService: WookieService
+    private wookieService: WookieService,
   ) { }
 
   ngOnInit(): void {
@@ -46,4 +46,26 @@ export class HomeComponent implements OnInit {
     return genreList;
   }
 
+  // Responsive Grid Tile 1
+  columnNum = 4; //initial count
+  tileSize: number = 300; //one tile should have this width
+  // Responsive Grid Tile step 2
+  setColNum(){
+    let width: any = window.document.getElementById('gridList')?.offsetWidth;
+    this.columnNum = Math.trunc(width/this.tileSize);
+  }
+  // Responsive Grid Tile step 3
+  //calculating upon loading
+  async ngAfterViewInit() {
+    await this.delay(100);
+    this.setColNum();
+  }
+  // Responsive Grid Tile step 4
+  //recalculating upon browser window resize
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.setColNum();
+  }
+
+  private delay(ms: number) { return new Promise(resolve => setTimeout(resolve, ms)); }
 }
